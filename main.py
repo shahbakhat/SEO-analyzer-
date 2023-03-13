d8e84d0d0899ab6b3a514c3eee926853509b87ce
Shah Bakhat <shahbakhat008@gmail.com>
2023-03-13 19:10:08 +0000

    made soup of all the content

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import nltk
 from nltk.tokenize import word_tokenize
 nltk.download('stopwords')
 nltk.download('punkt')
-url = 'https://codeinstitute.net/ie/'
-soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-# print(soup.prettify())
+
+
+
+def seo_analysis(url):
+    not_found = []
+    found = []
+    response = requests.get(url)
+    if response.status_code != 200:
+        print("Error: Unable to access the website " )
+        return
+    soup = BeautifulSoup(response.content, 'html.parser')
