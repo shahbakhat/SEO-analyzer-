74207523c6cd56ed5018d0eae09118b9331f9bef
Shah Bakhat <shahbakhat008@gmail.com>
2023-03-13 14:41:14 +0000

    sent request and got response back

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import nltk
 from nltk.tokenize import word_tokenize
+
+url = 'https://codeinstitute.net/ie/'
+soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+print(soup.prettify())
