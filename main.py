08f61f2e63107aad1f262dc686796149cf3945b4
Shah Bakhat <shahbakhat008@gmail.com>
2023-03-13 15:31:25 +0000

    downloaded 'stopwords' and 'punkt'

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 import nltk
 from nltk.tokenize import word_tokenize
-
+nltk.download('stopwords')
+nltk.download('punkt')
 url = 'https://codeinstitute.net/ie/'
 soup = BeautifulSoup(requests.get(url).text, 'html.parser')
 # print(soup.prettify())
